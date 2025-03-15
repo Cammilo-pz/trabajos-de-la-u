cbab9df3ff7a03b1acca8db9eb94deb232ee4dfa
cammilo-pz <pcamilo312021jose@gmail.com>
2025-03-15 15:42:20 -0500

    punto 4

--- TALLER 1/mcd y mcm.py
+++ TALLER 1/mcd y mcm.py
@@ -1,7 +1,7 @@
-n1= int(input("Ingrese un número para el primer valor: "))
-n2= int(input("Ingrese un número para el segundo valor: "))
-if n1%2 == 0 and n2%2 == 0:
-    if n1 < n2:
+n1= int(input("Ingrese un número para el primer valor: ")) #primer numero
+n2= int(input("Ingrese un número para el segundo valor: ")) #segundo numero
+if n1%2 == 0 and n2%2 == 0: # se crea la condición inical de ambos pares
+    if n1 < n2: #se verifica el mcd
         for i in range(1, n1 + 1, 1):
             if n1 % i == 0 and n2 % i == 0:
                 mcd = i
@@ -14,9 +14,9 @@ if n1%2 == 0 and n2%2 == 0:
                 i = i + 1
         else:
             mcd = n1
-    print(mcd)
-elif n1%2 != 0 and n2%2 != 0:
-        if n1 < n2:
+    print(("mcd: ") and mcd)
+elif n1%2 != 0 and n2%2 != 0: #se crea la condición ambos impares
+        if n1 < n2: #se verifica el mcd
             for i in range(1, n1 + 1, 1):
                 if n1 % i == 0 and n2 % i == 0:
                     mcd = i
@@ -29,9 +29,9 @@ elif n1%2 != 0 and n2%2 != 0:
                     i = i + 1
             else:
                 mcd = n1
-        print(mcd)
-else: 
-        if n1 != n2:
+        print(("mcd: ") and mcd)
+else: # se genera de por sí la condición par impar o viceversa
+        if n1 != n2: #se verifica el mcm
             if n1 > n2:
                 max = n1
             else:
@@ -39,7 +39,7 @@ else:
             for i in range(n1 * n2, max - 1, -1):
                 if i % n1 == 0 and i % n2 == 0:
                     mcm = i
-            print(mcm)
+            print(("mcm: ") and mcm)
         else:
             mcm = n1
  
