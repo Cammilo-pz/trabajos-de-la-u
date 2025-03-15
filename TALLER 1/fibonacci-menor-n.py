97b3447b5149b416f911fe0dc566d11bdc7b3829
cammilo-pz <pcamilo312021jose@gmail.com>
2025-03-15 15:04:03 -0500

    punto 9

--- TALLER 1/fibonacci-menor-n.py
+++ TALLER 1/fibonacci-menor-n.py
@@ -1,15 +1,15 @@
-n = int(input("Ingrese un número:"))
+n = int(input("Ingrese un número:")) # se ingresa un numero
 print("los números de la serie de fibonacci menores que "+ str(n)+ " son:")
-a = 0
+a = 0 #se imprimen los dos primeros
 b = 1
 print(a)
 print(b)
 i = a
-while i < n:
+while i < n: #se inicia un bucle en donde se hacen las operaciones
     suma = a + b
     a = b
     b = suma
-    if b < n:
-        print(b)
+    if b < n: #se coloca la condición para garantizar que sean menores que n
+        print(b)# se va imprimiendo cada numero menor
     else:
         i = n
