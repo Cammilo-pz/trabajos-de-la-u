db16d3325fe89f4b00a04385f10a27804528c4d9
cammilo-pz <pcamilo312021jose@gmail.com>
2025-03-15 14:59:52 -0500

    punto 10

--- TALLER 1/fibo_mayora.py
+++ TALLER 1/fibo_mayora.py
@@ -1,13 +1,13 @@
-n = int(input("Ingrese un número:"))
+n = int(input("Ingrese un número:"))# se ingresa un número
 print("el siguiente número de la serie de fibonacci más cercano a " + str(n)+" es:")
 a = 0
 b = 1
-i = ("comiemzo")
+i = ("comiemzo")#se crea una variable para iniciar el bucle
 while i == ("comiemzo"):
     suma = a + b
-    if n-suma<=0:
-        print(suma)
-        i = ("final")
+    if n-suma<=0: # el numero de la serie mayor a n, será el que al restarselo a n va a dar un numero negativo 
+        print(suma)# se imprime
+        i = ("final")# se finaliza el bucle
     else:
         a = b
         b = suma
