8960189dc2c9ccc71024a0420a678206c05be030
cammilo-pz <pcamilo312021jose@gmail.com>
2025-03-15 15:06:22 -0500

    punto 8

--- TALLER 1/cifra+1.py
+++ TALLER 1/cifra+1.py
@@ -1,5 +1,5 @@
 n = int(input("Ingresa un numero: "))# ingresa un numero 
-#convertimos el valor entero en unaa lista
+#convertimos el valor entero en una lista
 num= (str(n))
 cifras = ""#se asigna la variable a imprimir
 #leemos desde el primer numero del entero ingresado, ya convertido a lista, hasta el último
@@ -7,6 +7,6 @@ for i in num:
     #se usa el residuo ya que el residuo de la suma de (9+1)%10 es 0 y de cualquier numero<10 es el mismo numero
     dig= ((int(i)+1)%10)#sumamos una unidad a la cifra que se toma de la lista
     cifras= cifras+str(dig)# vamos almacenando en cifras el nuevo numero ya sumado
-print(cifras)
+print(cifras)#se imprime el nuevo numero
 
 
