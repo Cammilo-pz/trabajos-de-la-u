7c498867b141b746f11ef2876cbf010ba3f06b26
cammilo-pz <pcamilo312021jose@gmail.com>
2025-03-15 15:55:11 -0500

    punto 1

--- TALLER 1/promedio de los pares.py
+++ TALLER 1/promedio de los pares.py
@@ -1,12 +1,12 @@
 #CANTIDAD DE NUMEROS QUE QUIERE INGRESAR
-n = int(input("¿Cuántos números dese ingresar:")) #PEDIMOS AL USUAR
+n = int(input("¿Cuántos números desea ingresar:")) 
 i = 0
 suma = 0
 pares = 0
 
 for i in range(1, n + 1):
-    elementos = int(input("Ingrese números:"))
-    if elementos % 2 == 0:
+    elementos = int(input("Ingrese números:"))# ingresa la cantidad de numeros establecida
+    if elementos % 2 == 0: #comprueba si es par
         suma = suma + elementos
         pares = pares + 1
-print(suma/pares)
+print("el promedio de los numeros pares es: ",(suma/pares))# se saca el promedio de solo los pares
